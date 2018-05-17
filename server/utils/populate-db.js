4efa1d70c0777429130353cfd3add7406cc1491b
Alexey Vishnyakov <n3tn0de@gmail.com>
2018-05-17 22:31:18 +0300

    Populate more users

--- server/utils/populate-db.js
+++ server/utils/populate-db.js
@@ -1,8 +1,24 @@
 import User from '../models/User'
+const chance = require('chance').Chance()
+
+const usersAmount = 10
+const devicesAmount = 2500
 
 export const populateUsers = () => {
   User.create({
     email: 'n3tn0de@gmail.com',
+    firstName: 'Alexey',
+    lastName: 'Vishnyakov',
+    canEdit: true,
     password: 'test'
   })
+  for (let index = 0; index <= usersAmount; index++) {
+    User.create({
+      email: chance.email(),
+      firstName: chance.first(),
+      lastName: chance.last(),
+      password: chance.string(),
+      canEdit: chance.bool(),
+    })
+  }
 }
