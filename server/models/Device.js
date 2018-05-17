bba8ced24e184904f28693793288af2b4811d587
Alexey Vishnyakov <n3tn0de@gmail.com>
2018-05-17 22:47:51 +0300

    Add device model

---
+++ server/models/Device.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const DeviceSchema = new mongoose.Schema({
+  ipv4: {
+    type: String,
+    match: [
+      /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
+      'is invalid',
+    ],
+  },
+  ipv6: {
+    type: String,
+    match: [
+      /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/,
+      'is invalid',
+    ],
+  },
+  mac: {
+    type: String,
+    match: [/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/, 'is invalid'],
+    index: true,
+  },
+  name: {
+    type: String,
+    index: true,
+  },
+  description: String,
+  group: String,
+  office: Number,
+  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
+  os: String,
+}, {timestamps: true})
+
+export default mongoose.model('Device', DeviceSchema)
