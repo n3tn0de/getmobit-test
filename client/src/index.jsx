504160bc75c4f6908b57fa73802a474a436a2c02
Alexey Vishnyakov <n3tn0de@gmail.com>
2018-05-24 11:23:16 +0300

    Move redux and router to index, add reducers hot reload

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,17 +3,53 @@ import ReactDOM from 'react-dom'
 
 import { AppContainer } from 'react-hot-loader';
 
+import { Route, Link } from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import {
+  ConnectedRouter,
+  routerReducer,
+  routerMiddleware,
+} from 'react-router-redux';
+
+import * as reducers from './reducers';
+
+import apiMiddleware from './middleware/api'
+
 import { attach as attachFastClick } from 'fastclick'
 import 'normalize.css'
 
 import Root from './containers/Root/Root'
+
+const reducer = combineReducers({
+  ...reducers,
+  router: routerReducer,
+});
+
+const history = createHistory()
+
+const middleware = [
+  apiMiddleware,
+  routerMiddleware(history),
+];
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+);
 
 attachFastClick(document.body)
 
 const render = Component => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Component />
+        </ConnectedRouter>
+      </Provider>
     </AppContainer>,
     document.getElementById(`getmobit`)
   )
@@ -22,8 +58,16 @@ const render = Component => {
 render(Root)
 
 if (module.hot) {
-  module.hot.accept('./containers/Root/Root.jsx', () => {
+  module.hot.accept('./containers/Root/Root', () => {
     const NextRootContainer = require('./containers/Root/Root').default;
     render(NextRootContainer);
+  });
+  module.hot.accept('./reducers/index', () => {
+    const nextRootReducer = require('./reducers/index');
+    const reducer = combineReducers({
+      ...nextRootReducer,
+      router: routerReducer,
+    });
+    store.replaceReducer(reducer);
   });
 }
